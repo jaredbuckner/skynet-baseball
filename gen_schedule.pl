af5300366da8006c7e2bb5cc57f2c7560d583879
Jared Buckner <jbuckner@qti.qualcomm.com>
2016-03-23 15:28:01 -0400

    Created a schedule-generator

---
+++ gen_schedule.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+use lib ".";
+use Player;
+
+Player->fillData('data');
+
+my ($Weeks) = @ARGV;
+
+$Weeks //= 20;
+
+my @Teams = Player->allOwners();
+
+if(@Teams % 2 != 0) { push(@Teams, "<bye>"); }
+
+my @LIdx = (0..(@Teams - 1));
+
+for(my $Week = 1; $Week <= $Weeks; ++$Week) {
+    print "==== WEEK $Week ====\n";
+
+    for(my $GIdx = 0; $GIdx != @LIdx / 2; ++$GIdx) {
+        if($GIdx == 0 ? $LIdx[-1] % 2 == 1 : $GIdx % 2 == 1) {
+            printf("%30s @ %-30s\n",
+                   $Teams[$LIdx[$GIdx]],
+                   $Teams[$LIdx[@LIdx - $GIdx - 1]]);
+        } else {
+            printf("%30s @ %-30s\n",
+                   $Teams[$LIdx[@LIdx - $GIdx - 1]],
+                   $Teams[$LIdx[$GIdx]]);
+        }
+    }
+
+    splice(@LIdx, 1, 0, splice(@LIdx, -1, 1));
+}
+
+
+    
